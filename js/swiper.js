5c9d2ff4491964b59b148de1f3570278caa285a4
Maxim <shafeevms@gmail.com>
2023-03-13 22:57:30 +0300

    swiper added

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+const headerSwiper = new Swiper('.hero__swiper', {
+  autoplay: true,
+  speed: 4000,
+  loop: true,
+  effect: 'fade',
+  parallax: true,
+});
+
+
