b2f874ea0d9876d956e3a2913725e7b3c73cac25
Maxim <shafeevms@gmail.com>
2023-03-16 18:40:38 +0300

    footer draft is done

--- js/accordion.js
+++ js/accordion.js
@@ -2,8 +2,8 @@ const buttons = document.querySelectorAll('.accordion__header');
 
 Array.from(buttons).forEach(btn => {
   btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const content = e.target.nextElementSibling;
+    const parent = e.target.closest('.accordion__item');
+    const content = parent.querySelector('.accordion__body');
     content.classList.toggle('close');
   })
-})
+});
