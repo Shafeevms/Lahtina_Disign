806518113ef433b6e6d63366f710ea3b53820005
Maxim <shafeevms@gmail.com>
2023-03-28 19:51:58 +0300

    team variant

--- js/animations/team_position.js
+++ js/animations/team_position.js
@@ -1,60 +1,71 @@
-import { gsap } from 'gsap';
+import {gsap} from 'gsap';
 
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {MorphSVGPlugin} from 'gsap/MorphSVGPlugin';
 
 gsap.registerPlugin(ScrollTrigger, MorphSVGPlugin);
 
-const svg1 = gsap.timeline({ repeat: -1, yoyo: true, ease: "power4.out" });
-const svg2 = gsap.timeline({ repeat: -1, yoyo: true, ease: "power3.out" });
+const svg1 = gsap.timeline({repeat: -1, yoyo: true, ease: 'power4.out'});
+const svg2 = gsap.timeline({repeat: -1, yoyo: true, ease: 'power3.out'});
 
 svg1
-  .to('.step11', { morphSVG: '.step12', duration: 1.2 })
-  .to('.step11', { morphSVG: '.step13', duration: 1.4 }, '>')
-  .to('.step11', { morphSVG: '.step14', duration: 1 }, '>')
-  .to('.step11', { morphSVG: '.step15', duration: 1.4 });
+  .to('.step11', {morphSVG: '.step12', duration: 1.2})
+  .to('.step11', {morphSVG: '.step13', duration: 1.4}, '>')
+  .to('.step11', {morphSVG: '.step14', duration: 1}, '>')
+  .to('.step11', {morphSVG: '.step15', duration: 1.4});
 
 svg2
-  .to('.step21', { morphSVG: '.step25', duration: 1.4, delay: 4 })
-  .to('.step21', { morphSVG: '.step23', duration: 1 }, '>')
-  .to('.step21', { morphSVG: '.step22', duration: 1.3 })
-  .to('.step21', { morphSVG: '.step24', duration: 1.3 }, '>')
-  .to('.step21', { morphSVG: '.step25', duration: 1.4, delay: 4, scale: 0.9, ease: 'Sine.easeIn' })
-  .to('.step21', { morphSVG: '.step23', duration: 1 }, '>')
-  .to('.step21', { morphSVG: '.step22', duration: 1.3 })
-  .to('.step21', { morphSVG: '.step24', duration: 1.3 }, '>')
-// const title = document.querySelector('.team__title');
-// const leader = document.querySelector('.leader');
+  .to('.step21', {morphSVG: '.step25', duration: 1.4, delay: 4})
+  .to('.step21', {morphSVG: '.step23', duration: 1}, '>')
+  .to('.step21', {morphSVG: '.step22', duration: 1.3})
+  .to('.step21', {morphSVG: '.step24', duration: 1.3}, '>')
+  .to('.step21', {morphSVG: '.step25', duration: 1.4, delay: 4, scale: 0.9, ease: 'Sine.easeIn'})
+  .to('.step21', {morphSVG: '.step23', duration: 1}, '>')
+  .to('.step21', {morphSVG: '.step22', duration: 1.3})
+  .to('.step21', {morphSVG: '.step24', duration: 1.3}, '>');
+
+const title = document.querySelector('.team__title');
+const leader = document.querySelector('.leader');
 
 
-//
-// const tlTeam = gsap.timeline({
-//   ease: 'Sine.easeIn',
-//   scrollTrigger: {
-//     markers: true,
-//     trigger: '.team',
-//     start: 'top 80%',
-//     end: 'top -15%',
-//     toggleActions: 'play reverse play reverse'
-//   }
-// });
-//
-// tlTeam
-//   .to(title, {
-//     duration: 1,
-//     // y: '30vh',
-//     yPercent: 50,
-//   })
-//   .to(title, {
-//     duration: 0.4,
-//     opacity: 0
-//   })
-//   .to(leader, {
-//     duration: 2,
-//     // x: '40vw',
-//     // y: '-30vh',
-//     yPercent: 50,
-//     xPercent: 50,
-//     scaleX: 1.5,
-//     scaleY: 1.5,
-//   })
+ScrollTrigger.create({
+  trigger: '.team',
+  // markers: true,
+  start: 'top 80%',
+  end: 'center center',
+  onEnter: () => {
+    gsap.to(title, {
+      duration: 1.5,
+      y: '10vh',
+    });
+    moveEl('.team__item_1', '20vw');
+    moveEl('.team__item_2', '15vw');
+    moveEl('.team__item_3', '-20vw');
+    moveEl('.team__item_4', '-15vw');
+    moveEl('.team__item_5', '-20vw');
+    moveEl(leader, '0', '-20vh');
+  }
+});
+
+
+const moveEl = (el, x = '0', y = '0') => {
+  gsap.fromTo(el, {
+      opacity: 0,
+    },
+    {
+      duration: 1.5,
+      opacity: 1,
+      x,
+      y
+    });
+};
+
+gsap.to('.service', {
+  backgroundColor: '#262525',
+  scrollTrigger: {
+    trigger: '.service',
+    markers: true,
+    start: 'top 80%',
+    end: 'center center',
+  }
+})
