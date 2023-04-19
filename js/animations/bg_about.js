8fbb4bdb8fe13354967ce73718e1f7c5f2796363
Maxim <shafeevms@gmail.com>
2023-04-19 21:08:29 +0300

    about540

--- js/animations/bg_about.js
+++ js/animations/bg_about.js
@@ -4,25 +4,58 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const bgTimeline = gsap.timeline({
-  ease: 'power2.out',
-  paused: true,
-});
+const mm = gsap.matchMedia();
+const mediaQuery = window.matchMedia('(max-width: 540px)');
+const aboutSection = document.querySelector('.about');
+const customSpeedElements = Array.from(aboutSection.querySelectorAll('[data-speed]'));
+const tempSpeedArray = ['0.6', '1.8', '1.1', '1.1', '0.88', '0.93'];
 
-bgTimeline
-  .to('.about', {
-    backgroundColor: '#1B2645',
-    duration: 4
+mm.add('(min-width: 540px)', () => {
+  const bgTimeline = gsap.timeline({
+    ease: 'power2.out',
+    paused: true,
   });
 
-ScrollTrigger.create({
-  // markers: true,
-  trigger: '.about',
-  start: '100 center',
-  end: 'bottom bottom',
-  scrub: true,
-  onUpdate: (self) => {
-    const progress = +self.progress.toFixed(2);
-    bgTimeline.progress(progress );
-  },
+  bgTimeline
+    .to('.about', {
+      backgroundColor: '#1B2645',
+      duration: 4
+    });
+
+  ScrollTrigger.create({
+    // markers: true,
+    trigger: '.about',
+    start: '100 center',
+    end: 'bottom bottom',
+    scrub: true,
+    onUpdate: (self) => {
+      const progress = +self.progress.toFixed(2);
+      bgTimeline.progress(progress );
+    },
+  });
 });
+
+mediaQuery.addEventListener('change', e => {
+  if (e.matches) {
+    customSpeedElements.forEach((el, index) => {
+      el.dataset.speed = '1';
+    });
+  } else {
+    customSpeedElements.forEach((el, index) => {
+      el.dataset.speed = String(tempSpeedArray[index]);
+    });
+  }
+});
+
+window.addEventListener('load', () => {
+  if (mediaQuery.matches) {
+    console.log(1);
+    customSpeedElements.forEach((el) => {
+      console.log(2);
+      el.dataset.speed = '1';
+    });
+  }
+})
+
+//TODO решить вопрос с data-speed при load страницы
+// https://greensock.com/docs/v3/Plugins/ScrollSmoother
--- js/animations/title_wrapper.js
+++ js/animations/title_wrapper.js
@@ -5,21 +5,24 @@ const line2 = document.querySelector('.line__2');
 const line3 = document.querySelector('.line__3');
 const line4 = document.querySelector('.line__4');
 
-const tlTitleWrapper = gsap.timeline({
-  duration: 0.5,
-  ease: 'Sine.easeIn',
-  scrollTrigger: {
-    // markers: true,
-    trigger: '.about',
-    start: 'top 80%',
-    end: 'top -15%',
-    toggleActions: 'play reverse play reverse'
-  }
+const mm = gsap.matchMedia();
+
+mm.add('(min-width: 540px)', () => {
+  const tlTitleWrapper = gsap.timeline({
+    duration: 0.5,
+    ease: 'Sine.easeIn',
+    scrollTrigger: {
+      // markers: true,
+      trigger: '.about',
+      start: 'top 80%',
+      end: 'top -15%',
+      toggleActions: 'play reverse play reverse'
+    }
+  });
+
+  tlTitleWrapper
+    .to(line1, {height: '100%'})
+    .to(line2, {width: '100%'})
+    .to(line3, {height: '100%'})
+    .to(line4, {width: '100%'});
 });
-
-
-tlTitleWrapper
-  .to(line1, {height: '100%'})
-  .to(line2, {width: '100%'})
-  .to(line3, {height: '100%'})
-  .to(line4, {width: '100%'});
